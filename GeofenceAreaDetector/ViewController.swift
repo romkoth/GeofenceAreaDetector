725be900baf53f5f3f1ec84cacc0ddb4860f8b0b
Roman Bobelyuk <bobelyukr@gmail.com>
2018-12-24 23:16:51 +0200

    Initial Commit

---
+++ GeofenceAreaDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeofenceAreaDetector
+//
+//  Created by Roman Bobelyuk on 12/24/18.
+//  Copyright © 2018 Roman Bobelyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
